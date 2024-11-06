ce993dfca9108d18e9e67a8cdbd9024d8a78ad9a
lsw <hkbvc1222@gmail.com>
2024-11-07 06:01:31 +0900

    chore: alias

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ export default mergeConfig(
     },
     resolve: {
       alias: {
+        '@': path.resolve(__dirname, 'src'),
         '@atoms': path.resolve(__dirname, 'src/components/atoms'),
         '@molecules': path.resolve(__dirname, 'src/components/molecules'),
         '@organisms': path.resolve(__dirname, 'src/components/organisms'),
